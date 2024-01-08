8b85f3e8c97554c330bcfae72d3214fc7be01724
Farhan Basheer <farhanfb6@outlook.com>
2024-01-08 21:20:56 +0500

    Minor Cleanups

--- AccountApp/urls.py
+++ AccountApp/urls.py
@@ -5,6 +5,7 @@ app_name = 'AccountApp'
 
 urlpatterns = [
     path('login/', views.user_login, name='custom_login'),
+    path('logout/', views.user_login, name='ViewLogout'),
     #path('password/reset/', views.CustomPasswordResetView.as_view(), name='custom_password_reset'),
     
 ]
--- AccountApp/views.py
+++ AccountApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def user_login(request):
     if request.method == 'POST':
@@ -13,11 +13,17 @@ def user_login(request):
         if user is not None:
             # Login the authenticated user
             login(request, user)
-
-            # Redirect to a success page or homepage
-            return redirect('core:ViewDetectPerson')
+            if user.is_superuser:
+                return redirect('core:index')
+            else:
+                # Redirect to a success page or homepage
+                return redirect('core:ViewDetectPerson')
         else:
             # Handle authentication failure, e.g., show an error message
             return render(request, 'AccountApp/login_form.html', {'error_message': 'Invalid credentials'})
 
     return render(request, 'AccountApp/login_form.html')
+
+def logoutView(request):
+    logout(request)
+    return redirect('core:Home')
